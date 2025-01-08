40eb4cc1e6675c192303775b88c2607f69235e54
Prachiti <prachitiagarkar1@gmail.com>
2025-01-08 16:14:13 +0530

    Practice questions of java

---
+++ Practice.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Practice{
+static int method(int a){
+return 5+a;
+}
+
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.print(method(4));
+//add(6,7);
+}
+}
+  /*int arr[]=new int[5];
+ System.out.print("enter nos: ");
+  for(int i=0;i<5;i++){
+   arr[i]=sc.nextInt();
+}
+
+for(int i=0; i<arr.length; i++){
+ System.out.print(arr[i]+ " ");
+}*/
+
+//ternary operator
+/*int a=10;
+int b=20
+int c=30;
+int d=7;
+int e=40;
+int max=(a>b && a>c && )?a:b;
+System.out.println("Max: " +max);*/
+
